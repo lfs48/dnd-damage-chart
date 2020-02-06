ede1e3d089f779425692fb5b336dd12443176b6b
lucas schraier <lucasfschraier@gmail.com>
2020-02-05 23:48:46 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+      <div className="app-container">
+          <header>{"D&D 5E Damage Chart Tool"}</header>
+      </div>
+    );
+}
+
+export default App;
