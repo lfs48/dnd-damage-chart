e8fbb9bb64bc28fcb83192fcc33224a84d313908
lucas schraier <lucasfschraier@gmail.com>
2020-02-07 13:28:11 -0500

    Implement controlled inputs through react state

--- src/components/character.jsx
+++ src/components/character.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {dndClasses, subclasses} from '../data/dndclasses';
+import {merge} from 'lodash';
 
 const Character = () => {
+
+    const [state, setState] = useState({
+        class: "Artificer"
+    });
+
+    const updateInput = (event, field) => {
+        event.preventDefault();
+        const newState = merge({}, state);
+        newState[field] = event.target.value;
+        setState(newState);
+    }
 
     const classOptions = dndClasses.map( (name,i) => {
         return <option value={name} key={i}>{name}</option>
@@ -13,6 +25,8 @@ const Character = () => {
             <label htmlFor="classSelect">Class</label>
             <select 
                 id="classSelect"
+                value={state.class}
+                onChange={e => updateInput(e, 'class')}
             >
                 {classOptions}
             </select>
