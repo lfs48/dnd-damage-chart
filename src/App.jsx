3c51b7b5d910c99d39c0bcc0d9606051e850441e
lucas schraier <lucasfschraier@gmail.com>
2020-02-06 23:52:46 -0500

    Render params component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/navbar';
+import Params from './components/params';
 
 const App = () => {
   return (
     <div className="app-container">
       <Navbar/>
+      <Params />
     </div>
   );
 };
