488963a84fae50e0077e9fc0c52a6dc83724bc2e
lucas schraier <lucasfschraier@gmail.com>
2020-02-07 13:10:05 -0500

    Render character component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/navbar';
 import Params from './components/params';
+import Character from './components/character';
 
 const App = () => {
   return (
     <div className="app-container">
       <Navbar/>
+      <Character />
       <Params />
     </div>
   );
