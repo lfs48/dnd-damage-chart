8e01759f713e0c3f0e7dd7b9b4c04ef9097ca79c
lucas schraier <lucasfschraier@gmail.com>
2020-02-06 23:10:50 -0500

    Initial commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className="nav-container">
+            <header>{`D&D 5E Damage Visualization Tool`}</header>
+        </nav>
+    );
+}
+
+export default Navbar;
