3a4816746584fd24dec3495ff6d69aaf48c9fb00
lucas schraier <lucasfschraier@gmail.com>
2020-02-12 13:32:35 -0500

    Add attributes from options argument

--- src/models/dnd_class.js
+++ src/models/dnd_class.js
@@ -1,6 +1,11 @@
 class DndClass {
     constructor(options) {
-        features = options.features;
+        let key, val;
+        Object.entries(options).map( (obj) => {
+            key = obj[0];
+            val = obj[1];
+            this[key] = val;
+        });
     }
 }
 
