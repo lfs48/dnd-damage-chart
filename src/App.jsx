a53958e9fc49957db12973604c2293ceebb34369
lucas schraier <lucasfschraier@gmail.com>
2020-02-11 20:18:20 -0500

    Render chart component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Navbar from './components/navbar';
 import Params from './components/params';
 import Character from './components/character';
+import Chart from './components/chart';
 
 const App = () => {
   return (
     <div className="app-container">
       <Navbar/>
+      <Chart/>
       <Character />
       <Params />
     </div>
