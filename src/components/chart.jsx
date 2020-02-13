cffc1964e0c41911d4b488c6398e53fbf1fce2f7
lucas schraier <lucasfschraier@gmail.com>
2020-02-13 16:43:07 -0500

    Remove canvasJS

--- src/components/chart.jsx
+++ src/components/chart.jsx
@@ -1,37 +1,11 @@
 import React from 'react';
-import CanvasJSReact from '../assets/canvasjs.react';
-const CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 
 const Chart = () => {
 
-    const options = {
-        animationEnabled: true,
-        theme: "light2",
-        title:{
-            text: "Damage of each Fighter Subclass"
-        },
-        axisX: {
-            title: "Subclass",
-        },
-        axisY: {
-            title: "Damage"
-        },
-        data: [{
-            type: "column",
-            dataPoints: [
-                { y:  1, label: "Cavalier" },
-                { y:  2, label: "Champion" },
-                { y:  3, label: "Arcane Archer" },
-                { y:  4, label: "Eldritch Knight" },
-                { y:  5, label: "Battlemaster" }
-            ]
-        }]
-    };
-
     return(
         <section id="chart-container">
-            <CanvasJSChart options={options} />
+            <span>testing</span>
         </section>
     );
 }
