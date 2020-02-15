1be0c445d52d39b8de545ff3ac7d88657a57cf55
lucas schraier <lucasfschraier@gmail.com>
2020-02-14 22:36:27 -0500

    Render recharts bar graph component

--- src/components/chart.jsx
+++ src/components/chart.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
-
+import {BarChart, Bar, XAxis, YAxis} from 'recharts';
 
 const Chart = () => {
 
+    const data = [
+        {
+            name: "Battlemaster",
+            pv: 200,
+        },
+        {
+            name: "Champion",
+            pv: 180,
+        },
+        {
+            name: "Eldritch Knight",
+            pv: 190,
+        },
+    ];
+
     return(
         <section id="chart-container">
-            <span>testing</span>
+            <BarChart width={730} height={250} data={data} >
+                <XAxis dataKey="name"/>
+                <YAxis />
+                <Bar barSize={100} dataKey="pv" />
+            </BarChart>
         </section>
     );
 }
